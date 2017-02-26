2dbb9bd6a368237f75ab9ce5528d26d34cfaa0e5
Aaron Mitchell <aaronmitchellart@gmail.com>
2017-02-26 15:03:57 -0500

    building out about page

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router';
 import Helmet from "react-helmet";
 
 export default class About extends Component {
@@ -17,6 +18,35 @@ export default class About extends Component {
         <h2>
         About
         </h2>
+        <div className="about__info">
+          <p>
+            <span className="p__highlight">Darren Schillaci</span> has been building guitars for over twenty years. A lifelong player, he turns his passion into an unmatched precision that resonates from every guitar. When you order from Schillaci Guitars, you are guaranteed to receive a one of a kind instrument.
+          </p>
+          <p>
+            All guitars are made 100% in house by one person, handcrafted to meet even the most particular player's needs. If you're not ready for the commitment of your own Schillaci guitar, consider replacing a lower grade import neck with an american made Schillaci neck to truly feel the difference.
+          </p>
+          <p>
+            All guitar bodies are painted with sparkling Dupont paint (matched colors available upon request). Figured tops and bodies, natural woods, stains and sunburst finishes are also available.
+          </p>
+          <p>
+            <span className="p__highlight">If you are interested in ordering a guitar or neck,</span> <Link to="/contact">continue to the contact page where you can send us an email.</Link>
+          </p>
+          <p>
+            Photography by
+          </p>
+          <p>
+            Tina Guay guayphotography@gmail.com
+          </p>
+          <p>
+            Bill Harbold eharbold@comcast.net
+          </p>
+          <p>
+            Website Design by <a href="http://www.mitchellaaron.com">Aaron Mitchell.</a>
+          </p>
+        </div>
+        <div className="about__picture">
+          <img src="../../img/About/Darren_Schillaci_Portrait_02.jpg" />
+        </div>
       </div>
     )
   }
