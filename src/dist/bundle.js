0eb820c98aa7d7c8ac66c1f824b64455dee76cd3
Aaron Mitchell <aaronmitchellart@gmail.com>
2017-02-27 21:54:38 -0500

    adding all guitars to constants

--- src/dist/bundle.js
+++ src/dist/bundle.js
@@ -29859,6 +29859,7 @@
 	});
 	var guitars = exports.guitars = [{
 	  name: "LSG",
+	  type: "guitar",
 	  about: ["1 5/8 thick 2pc. Mahogany body", "3pc. Mahogany neck w/ rosewood fingerboard", "24 5/8 short scale neck", "Headstock/ Pickguard match w/ matching truss cover plate", "Single pickup / Single volume control configuration", "Standard stop bar and tailpiece", "Side mounted output jack", "\"Greenglow\" red mahogany finish"],
 	  slider_photo: "Guitars/schillaci_guitars_slider_lsg_guitar_1.png",
 	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_lsg_guitar_1.jpg",
@@ -29866,10 +29867,131 @@
 	  hidden: false
 	}, {
 	  name: "Knucklehead",
+	  type: "guitar",
 	  about: ["One Piece Ash Body", "72 Matching Stained Gloss Finish", "Competition Racing Stripes", "Rosewood Neck with Skunk Stripe", "Black Tusq Nut", "Reverse Tremolo", "Vintage Bridge Plate with Custom Saddles", "Humbuckers with Individual Tone Controls", "Elongated Pickguard with Increased Spacing Between Volume and Tone Knobs", "Top Mounted Jack Cup"],
 	  slider_photo: "Guitars/schillaci_guitars_slider_knucklehead_guitar_1.jpg",
 	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_knucklehead_guitar_1.jpg",
 	  detail_photos: ["Guitar_Details/schillaci_guitars_detail_knucklehead_guitar_b_01.jpg", "Guitar_Details/schillaci_guitars_detail_knucklehead_guitar_b_02.jpg"],
+	  hidden: false
+	}, {
+	  name: "Knucklehead",
+	  type: "guitar",
+	  about: ["One Piece Mahogany Ribbon Body with Black Binding", "Natural Hand Rubbed Satin Finish", "Vintage Laotian Ebony Neck", "Black Tusq Nut", "24 Fret Short Scale", "Hard Tail Bridge with Black Hardware", "Alternating Bobbin Humbuckers", "Two Volume and Two Tone Controls with Blade Switch", "Side Mounted Jack"],
+	  slider_photo: "Guitars/schillaci_guitars_slider_knucklehead_guitar_2.jpg",
+	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_knucklehead_guitar_2.jpg",
+	  detail_photos: ["Guitar_Details/schillaci_guitars_detail_knucklehead_guitar_c_01.jpg", "Guitar_Details/schillaci_guitars_detail_knucklehead_guitar_c_02.jpg"],
+	  hidden: false
+	}, {
+	  name: "Knucklehead",
+	  type: "guitar",
+	  about: ["Multi-laminated body", "Tusq nut", "22 fret long scale", "Hard tail bridge", "Two volume, two tone with blade switch", "Side mounted jack"],
+	  slider_photo: "Guitars/schillaci_guitars_slider_knucklehead_guitar_3.jpg",
+	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_knucklehead_guitar_4.jpg",
+	  detail_photos: ["Guitar_Details/schillaci_guitars_detail_knucklehead_guitar_a_01.jpg", "Guitar_Details/schillaci_guitars_detail_knucklehead_guitar_a_01.jpg"],
+	  hidden: false
+	}, {
+	  name: "Knucklehead",
+	  type: "guitar",
+	  about: ["Three Piece Ash Body", "Flame Maple Gloss Finish", "Matching Seafoam Green Peghead, Pickguard and Back", "Chambered with F Hole", "One Piece Maple Neck with Skunk Stripe", "White Tusq Nut", "Sperzel Locking Tuners", "Vintage Reverse Tremolo with Modern Saddles", "Chrome Covered Humbuckers", "Elongated Pickguard", "Top Mounted Jack Cup"],
+	  slider_photo: "Guitars/schillaci_guitars_slider_knucklehead_guitar_4.jpg",
+	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_knucklehead_guitar_3.jpg",
+	  detail_photos: ["Guitar_Details/schillaci_guitars_detail_knucklehead_guitar_d_01.jpg", "Guitar_Details/schillaci_guitars_detail_knucklehead_guitar_d_02.jpg", "Guitar_Details/schillaci_guitars_detail_knucklehead_guitar_d_03.jpg"],
+	  hidden: false
+	}, {
+	  name: "Knucklehead",
+	  type: "guitar",
+	  about: ["Solid Maple Body", "Solid Chartreuse Yellow Gloss Finish", "Rosewood Finger Board Non Skunk Stripe Neck", "Upper Tross Rod Adjustment", "Vintage Reverse Tremolo", "Chrome Covered Humbuckers with Individual Tone Controls", "Elongated Pickguard with Increased Spacing Between Volume and Tone Knobs", "Top Mounted Jack Cup"],
+	  slider_photo: "Guitars/schillaci_guitars_slider_knucklehead_guitar_5.jpg",
+	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_knucklehead_guitar_5.jpg",
+	  detail_photos: ["Guitar_Details/schillaci_guitars_detail_knucklehead_guitar_e_01.jpg", "Guitar_Details/schillaci_guitars_detail_knucklehead_guitar_e_02.jpg"],
+	  hidden: false
+	}, {
+	  name: "Hammerhead",
+	  type: "guitar",
+	  about: ["Three Piece Mahogany Body", "Solid Seafoam Green Gloss Finish", "Matching Peghead", "Gabon Ebony Finger Board with Pearl Dots", "Graf-Tech Nut", "Reverse Vintage Tremolo with Graf-Tech Saddles", "Three Single Coil Pickups", "Elongated Pickguard with Increased Spacing Between Volume and Tone Knobs", "Top Mounted Jack Cup"],
+	  slider_photo: "Guitars/schillaci_guitars_slider_hammerhead_guitar_1.jpg",
+	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_hammerhead_guitar_1.jpg",
+	  detail_photos: [],
+	  hidden: false
+	}, {
+	  name: "7 String Hammerhead",
+	  type: "guitar",
+	  about: ["One Piece Mahogany Ribbon Body", "Natural Hand Rubbed Satin Finish", "One Piece Maple Neck with Black Binding", "Micarta Nut", "Hard Tail Bridge with Black Hardware", "7 String Humbuckers with Individual Tone Controls", "Elongated Spacing Between Volume and Tone Knobs", "Top Mounted Jack Cup"],
+	  slider_photo: "Guitars/schillaci_guitars_slider_hammerhead_guitar_2.jpg",
+	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_hammerhead_guitar_2.jpg",
+	  detail_photos: ["Guitar_Details/schillaci_guitars_detail_hammerhead_guitar_a_01.jpg", "Guitar_Details/schillaci_guitars_detail_hammerhead_guitar_a_02.jpg"],
+	  hidden: false
+	}, {
+	  name: "Hammerhead",
+	  type: "guitar",
+	  about: ["1 PC. Pine body", "Clear \"GreenGlow\" finish", "22 fret rosewood neck", "Standard elongated pick guard", "Alnico sin/sin/sin pickup configuration", "w/ standard controls", "Top mounted jack cup", "Toggle \"kill\" switch"],
+	  slider_photo: "Guitars/schillaci_guitars_slider_hammerhead_guitar_3.jpg",
+	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_hammerhead_3.jpg",
+	  detail_photos: ["Guitar_Details/schillaci_guitars_detail_hammerhead_guitar_b_01.jpg", "Guitar_Details/schillaci_guitars_detail_hammerhead_guitar_b_02.jpg", "Guitar_Details/schillaci_guitars_detail_hammerhead_guitar_b_03.jpg"],
+	  hidden: false
+	}, {
+	  name: "Roy's Caster",
+	  type: "guitar",
+	  about: ["3 piece popular body", "Red gloss finish", "1 piece maple neck", "Matching peg head", "Three single coil pickups", "Elongated pick guard", "Top mounted jack cup"],
+	  slider_photo: "Guitars/schillaci_guitars_slider_royscaster_1.jpg",
+	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_royscaster_guitar_1.jpg",
+	  detail_photos: ["Guitar_Details/schillaci_guitars_detail_royscaster_a_01.jpg", "Guitar_Details/schillaci_guitars_detail_royscaster_a_02.jpg"],
+	  hidden: false
+	}, {
+	  name: "Riptide Bass I",
+	  type: "bass",
+	  about: ["2 PC. Popular body", "Red gloss finish", "Rosewood fingerboard", "Single J pickup", "Elongated vintage style pick guard", "Top mounted jack cup"],
+	  slider_photo: "Guitars/schillaci_guitars_slider_riptide_bass_1.jpg",
+	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_riptide_bass_1.jpg",
+	  detail_photos: ["Guitar_Details/schillaci_guitars_detail_riptide_bass_c_01.jpg", "Guitar_Details/schillaci_guitars_detail_riptide_bass_c_02.jpg", "Guitar_Details/schillaci_guitars_detail_riptide_bass_c_03.jpg"],
+	  hidden: false
+	}, {
+	  name: "Riptide Bass I",
+	  type: "bass",
+	  about: ["2 PC. Popular body", "Red gloss finish", "Rosewood fingerboard", "Single J pickup", "Elongated vintage style pick guard", "Top mounted jack cup"],
+	  slider_photo: "Guitars/schillaci_guitars_slider_riptide_bass_2.jpg",
+	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_riptide_bass_2.jpg",
+	  detail_photos: ["Guitar_Details/schillaci_guitars_detail_riptide_bass_a_01.jpg", "Guitar_Details/schillaci_guitars_detail_riptide_bass_a_02.jpg"],
+	  hidden: false
+	}, {
+	  name: "Riptide Bass II",
+	  type: "bass",
+	  about: ["Three Piece Alder Body", "Solid Antique White Gloss Finish", "Matching Peghead", "One Piece Maple Neck", "White Tusq Nut", "Two J Pickups", "Elongated Tortoise Shell Vintage Style Pickguard", "Top Mounted Jack Cup"],
+	  slider_photo: "Guitars/schillaci_guitars_slider_riptide_bass_3.jpg",
+	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_riptide_bass_3.jpg",
+	  detail_photos: ["Guitar_Details/schillaci_guitars_detail_riptide_bass_b_01.jpg", "Guitar_Details/schillaci_guitars_detail_riptide_bass_b_02.jpg"],
+	  hidden: false
+	}, {
+	  name: "Bosko Bass",
+	  type: "bass",
+	  about: ["One Piece Mahogany Ribbon Body", "Natural Hand Rubbed Satin Finish", "One Piece Maple Neck with Black Binding and Black Inlays", "Micarta Nut", "5 String Hard Tail Bridge with Black Hardware", "Single P Pickup", "Top Mounted Jack Cup"],
+	  slider_photo: "Guitars/schillaci_guitars_slider_bosko_bass_2.jpg",
+	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_bosko_bass_2.jpg",
+	  detail_photos: ["Guitar_Details/schillaci_guitars_detail_bosko_bass_b_01.jpg", "Guitar_Details/schillaci_guitars_detail_bosko_bass_b_02.jpg", "Guitar_Details/schillaci_guitars_detail_bosko_bass_b_03.jpg"],
+	  hidden: false
+	}, {
+	  name: "Bosko Bass",
+	  type: "bass",
+	  about: ["3 PC. Alder body ( east coast) sea green", "Maple neck w/ rosewood board", "Single P pickup", "Enlongated vintage style pick guard", "Top mounted jack cup"],
+	  slider_photo: "Guitars/schillaci_guitars_slider_bosko_bass_1.png",
+	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_bosko_bass_1.jpg",
+	  detail_photos: ["Guitar_Details/schillaci_guitars_detail_bosko_bass_a_01.jpg", "Guitar_Details/schillaci_guitars_detail_bosko_bass_a_02.jpg", "Guitar_Details/schillaci_guitars_detail_bosko_bass_a_03.jpg"],
+	  hidden: false
+	}, {
+	  name: "PJ Bass",
+	  type: "bass",
+	  about: ["70's style 1 PC. Maple neck w/ binding + big block inlays", "60's style Electronics w/ concentric pots + knobs", "50's style Basswood body w/ large face pick guard", "Hip shot string thru body bridge", "Flat black finish"],
+	  slider_photo: "Guitars/schillaci_guitars_slider_pj_bass_1.png",
+	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_pj_bass_1.png",
+	  detail_photos: ["Guitar_Details/schillaci_guitars_detail_pj_bass_01.jpg", "Guitar_Details/schillaci_guitars_detail_pj_bass_02.jpg", "Guitar_Details/schillaci_guitars_detail_pj_bass_03.jpg"],
+	  hidden: false
+	}, {
+	  name: "Kestral",
+	  type: "guitar",
+	  about: ["5 string orchestral guitar tuned in 5th's", "1 bass string & 4 guitar strings each with individual saddles", "24 fret. 26.5\" scale length", "Matching rosewood truss rod cover plate", "2 PC. Mahogany body", "Maple neck w/angled peghead", "Rosewood fingerboard", "Top mounted jack cup", "Neck bolt ferrules", "26.5\" scale length", "2 Lace Sensor humbuckers", "Coil tapped w/ push-pull tone pots"],
+	  slider_photo: "Guitars/schillaci_guitars_slider_kestral_guitar_1.jpg",
+	  thumb_photo: "Guitar_Thumbs/schillaci_guitars_thumb_kestral_guitar_1.jpg",
+	  detail_photos: ["Guitar_Details/schillaci_guitars_detail_kestral_guitar_a_01.jpg", "Guitar_Details/schillaci_guitars_detail_kestral_guitar_a_02.jpg", "Guitar_Details/schillaci_guitars_detail_kestral_guitar_a_03.jpg"],
 	  hidden: false
 	}];
 
