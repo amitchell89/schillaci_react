fdaa3e6973346a5302d85993fca32e6945d9e888
Aaron Mitchell <aaronmitchellart@gmail.com>
2017-03-22 21:43:41 -0400

    updating port on server

--- src/server.js
+++ src/server.js
@@ -41,8 +41,8 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname +'/index.html'));
 })
 
-app.listen(3000, function () {
-  console.log('Site listening on port 3000!')
+app.listen(3003, function () {
+  console.log('Site listening on port 3003!')
 })
 
 // handle contact page posts
