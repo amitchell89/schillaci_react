fdf1e667333183a2c9edeb00e9ebe6353513152d
Aaron Mitchell <aaronmitchellart@gmail.com>
2017-09-19 23:41:06 -0700

    upating price

--- src/store/constants/guitars.js
+++ src/store/constants/guitars.js
@@ -30,7 +30,7 @@ export const guitars = [
     name: "Knucklehead Esquire",
     type: "guitar",
     "style": "knucklehead",
-    price: "$2499.00",
+    price: "$799.00",
     about: [
       "1 PC. 1/4 sawn hard rock maple neck",
       "2 PC. popular body in Atlantic Sea Green",
